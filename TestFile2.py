ad1d83e9673ca084c838aa27ceb1497f0b6ccefc
JasonOsborn <47375292+JasonOsborn@users.noreply.github.com>
2019-09-16 13:50:53 -0500

    Update TestFile2.py

--- TestFile2.py
+++ TestFile2.py
@@ -41,6 +41,9 @@ COLORVARIANCE = 1024 # How many different colors can we display?
 ## Storage Variable Definition
 #Defines the file path /dev/fb1 as arbitrary variable named SDL_FBDEV for storage use.
 os.putenv('SDL_FBDEV', '/dev/fb1') 
+#Defines the USB file path as arbitrary variable named USB_Path
+os.putenv('USB_Path','')
+
 
 ## Device Initialization
 #Initialize screen
@@ -56,13 +59,71 @@ SensorArray = 768    ## REDEFINE for each sensor.
 # sensor 2: (x=32)*(y=24) = 768
 #sensor 3: ?
 
-SensorArrayX = SensorArray / 24  ## REDEFINE in terms of sensor's array X size, noted above.
-SensorArrayY = SensorArray / 32  ## REDEFINE in terms of sensor's array Y size, noted above.
+SensorArrayX = 24  ## REDEFINE in terms of sensor's array X size, noted above.
+SensorArrayY = 32  ## REDEFINE in terms of sensor's array Y size, noted above.
 
 points = [(math.floor(ix / SensorArrayX), (ix % SensorArrayY)) for ix in range(0, SensorArray)] # Creates array for containing SensorArray size
 
 
 ## 
-grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j] # Creates 2 grids from 0 to 7 w/ 32 points each, including 0 & 7 as points #Requires np
+grid_x, grid_y = numpy.mgrid[0:7:32j, 0:7:32j] # Creates 2 grids from 0 to 7 w/ 32 points each, including 0 & 7 as points #Requires np
 
 
+#Screen
+height = 240
+width = 240
+
+displayPixelWidth = width / 30
+displayPixelHeight = height / 30
+
+#Colors for the screen, from blue('indigo') to red
+
+colors = list(Color("indigo").range_to(Color("red"), COLORVARIANCE)) # requires color
+
+#create the array of colors
+colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors] # Redefines array colors in terms of RGB screen codes
+
+
+# Boot commands for the screen
+lcd = screen.display.set_mode((width, height))
+ 
+lcd.fill((255, 0, 0))
+ 
+screen.display.update()
+screen.mouse.set_visible(False)
+ 
+lcd.fill((0, 0, 0))
+screen.display.update()
+
+time.sleep(.1)
+
+# Initialize 
+Num = 0
+t0 = 0
+
+while True:
+ 
+    #read the pixels
+    pixels = []
+    for row in sensor.pixels:
+        pixels = pixels + row
+    pixels = [map_value(p, MINTEMP, MAXTEMP, 0, COLORVARIANCE - 1) for p in pixels]
+ 
+    #perform interpolation
+    #-> Interprets best fit for missing data?
+    bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
+ 
+    #draw everything onto screen
+    for ix, row in enumerate(bicubic):
+        for jx, pixel in enumerate(row):
+            screen.draw.rect(lcd, colors[constrain(int(pixel), 0, COLORVARIANCE- 1)],
+                             (displayPixelHeight * ix, displayPixelWidth * jx,
+                              displayPixelHeight, displayPixelWidth))
+ 
+    screen.display.update()
+    t1 = time.clock() - t0
+    
+    if buttonInput and t1 > 0.3:
+        t0 = time.clock()
+        screen.image.save(img, USB_Path + "image" + str(Num) + ".jpg")
+        Num = Num + 1
