3581565abe111aba8f4da271565e9574bef9110e
JasonOsborn <47375292+JasonOsborn@users.noreply.github.com>
2019-09-24 12:26:52 -0500

    Update TestFile2.py

--- TestFile2.py
+++ TestFile2.py
@@ -5,6 +5,7 @@
 # Determine use of map_value function
 #! Adafruit has it's own library- Do the others?
 #! buttonInput has no connected port. Important.
+#! Determine USB file path
 
 # Basic clock
 import time
@@ -14,6 +15,7 @@ import os
 import fnmatch
 import busio
 import board
+import smbus
 
 # Data manipulation
 import math
@@ -30,6 +32,30 @@ import adafruit_amg88xx
 # Define bus
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 
+## Dictionary for Sensors
+Sensor_Adafruit = {
+    'SensorName': 'ADA'
+    'SensorArray': 64
+    'SensorArrayX': 8
+    'SensorArrayY': 8
+}
+Sensor_MLX = {
+    'SensorName': 'MLX'
+    'SensorArray': 768
+    'SensorArrayX': 32 #24 or 32?
+    'SensorArrayY': 24 #24 or 32?
+}
+Sensor_FLIR = {
+    'SensorName': 'FLIR'
+    'SensorArray': 4800
+    'SensorArrayX': 80
+    'SensorArrayY': 60
+}
+
+##Choose from above sensors
+sensor = Sensor_Adafruit
+#sensor = Sensor_MLX
+#sensor = Sensor_FLIR
 
 
 ##Define Custom Functions
@@ -46,11 +72,10 @@ def Numbr(USB_Path)
                 return Num
             else
                 Num = Num + 1
-            
     return 0
 
 # Constrain Function
-#   Constraints value of temperature/color between defined bounds- Autoadjustment *is* possible, but not easy/ideal. Do anyway?
+#   Constraints value of color between defined bounds- Autoadjustment *is* possible, but not easy/ideal. Do anyway?
 def constrain(val, min_val, max_val):
     return min(max_val, max(min_val, val))
     
@@ -59,6 +84,13 @@ def constrain(val, min_val, max_val):
 def map_value(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
+def dataPoints():
+
+    for i,j in zip(range(x),range(y))
+
+    return pixelArray
+
+
 ## Thermal Variance Variabe Definition
 # Likely adjust these manually later on- Range is oddly small and oddly placed.
 MINTEMP = 26 #Minimum temperature, Blue
@@ -66,94 +98,100 @@ MAXTEMP = 32 #Maximum temperature, Red
 
 COLORVARIANCE = 1024 # How many different colors can we display?
 
+
 ## Storage Variable Definition
-#Defines the file path /dev/fb1 as arbitrary variable named SDL_FBDEV for storage use.
+# Defines the file path /dev/fb1 as arbitrary variable named SDL_FBDEV for storage use.
+    #Unused?
 os.putenv('SDL_FBDEV', '/dev/fb1') 
-#Defines the USB file path as arbitrary variable named USB_Path
-os.putenv('USB_Path','')
+#Defines the USB file path as arbitrary variable named ScreenCap
+os.putenv('ScreenCap','/media/pi/<HARD-DRIVE-LABEL>')
+    ##! Possibly incorect?
 
 ## Device Initialization
 #Initialize screen
 screen.init()
 
-#initialize sensor
-sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
-
-
-##Define sensor's output array
-SensorArray = 768    ## REDEFINE for each sensor. 
-# adafruit: (x=8)*(y=8) = 64
-# sensor 2: (x=32)*(y=24) = 768
-#sensor 3: ?
-
-SensorArrayX = 24  ## REDEFINE in terms of sensor's array X size, noted above.
-SensorArrayY = 32  ## REDEFINE in terms of sensor's array Y size, noted above.
-
-points = [(math.floor(ix / SensorArrayX), (ix % SensorArrayY)) for ix in range(0, SensorArray)] # Creates array for containing SensorArray size
+points = [(math.floor(ix / sensor['SensorArrayX']), (ix % sensor['SensorArrayY'])) for ix in range(0, sensor['SensorArray'])] # Creates array for containing SensorArray size
 
 
 ## 
-grid_x, grid_y = numpy.mgrid[0:7:32j, 0:7:32j] # Creates 2 grids from 0 to 7 w/ 32 points each, including 0 & 7 as points #Requires np
 
+if (sensor['SensorName'] != 'FLIR')
+    grid_x, grid_y = numpy.mgrid[0:7:32j, 0:7:32j] # Creates 2 grids from 0 to 7 w/ 32 points each, including 0 & 7 as points #Requires np [32x32]
+else
+    grid_x, grid_y = numpy.mgrid[0:7:80j, 0:7:60j] # Prooobably wrong.
 
-#Screen
+##Screen
+# To adjust number of pixels on screen, adjust height, width, or displayPixelHeight or displayPixelWidth
 height = 240
 width = 240
 
 displayPixelWidth = width / 30
 displayPixelHeight = height / 30
 
-#Colors for the screen, from blue('indigo') to red
+#Colors for the screen, from blue('indigo') to red. Number of jumps between is COLORVARIANCE
 
-colors = list(Color("indigo").range_to(Color("red"), COLORVARIANCE)) # requires color
+colors = list(Color("indigo").range_to(Color("red"), COLORVARIANCE)) # list of all colors btwn blue and red
 
-#create the array of colors
+#Redefines 'colors' into accessible RGB format
 colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors] # Redefines array colors in terms of RGB screen codes
 
 
 # Boot commands for the screen
-lcd = screen.display.set_mode((width, height))
+LiveCap = screen.display.set_mode((width, height))
  
-lcd.fill((255, 0, 0))
+LiveCap.fill((255, 0, 0))
  
 screen.display.update()
 screen.mouse.set_visible(False)
  
-lcd.fill((0, 0, 0))
+LiveCap.fill((0, 0, 0))
 screen.display.update()
 
 time.sleep(.1)
 
 # Initialize 
-Num = Numbr(USB_Path)
+Num = Numbr(ScreenCap)
 t0 = 0
 
+#Locate I2C active port
+active = [] # Create empty array 'active'
+active = [hex(x) for x in busio.I2C.scan()]
+
+if (len(active) == 1)
+    i2c_active_address = hex(busio.I2C.scan())
+else
+    exit() #Just close the program.
+    
+
 while True:
- 
+
     #read the pixels
-    pixels = []
-    for row in sensor.pixels:
-        pixels = pixels + row
-    pixels = [map_value(p, MINTEMP, MAXTEMP, 0, COLORVARIANCE - 1) for p in pixels]
- 
+    pixelArray = [] # Creates empty array
+    for row0 in sensor['SensorName'].pixels: #Defines array as containing 'sensor' data
+        pixelArray = pixelArray + row0 # Note '+' here is append, not add
+    pixelArray = [map_value(p, MINTEMP, MAXTEMP, 0, COLORVARIANCE - 1) for p in pixelArray] #Redefines 'pixels' data onto the 1024 range
+
     #perform interpolation
-    #-> Interprets best fit for missing data?
-    bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
- 
+    #-> Interprets Ax^2+Bx+C line of best fit for missing data
+    bicubic = griddata(points, pixelArray, (grid_x, grid_y), method='cubic')
+        
+
     #draw everything onto screen
-    for ix, row in enumerate(bicubic):
-        for jx, pixel in enumerate(row):
-            screen.draw.rect(lcd, colors[constrain(int(pixel), 0, COLORVARIANCE- 1)],
+    for ix, row1 in enumerate(bicubic):
+        for jx, pixel in enumerate(row1):
+            screen.draw.rect(LiveCap, colors[constrain(int(pixel), 0, COLORVARIANCE - 1)],
                              (displayPixelHeight * ix, displayPixelWidth * jx,
                               displayPixelHeight, displayPixelWidth))
- 
+
     screen.display.update()
-    
+
     ## Screenshot functionality
     # Debouncing + Initial Button Input:
     t1 = time.clock() - t0
     if buttonInput and t1 > 0.3:
         t0 = time.clock()
-        # Save a screenshot of the current screen @ USB_Path called image[Num].jpg where Num is an arbitrary number
-        screen.image.save(img, USB_Path + "image" + str(Num) + ".jpg")
+        # Save a screenshot of the current screen @ ScreenCap called image[Num].jpg where Num is an arbitrary number
+        screen.image.save(LiveCap, ScreenCap + "image" + str(Num) + ".jpg")
         Num = Num + 1
+#End
