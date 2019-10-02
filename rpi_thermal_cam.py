78dece4221201aeda9029f863f89ced122062fc1
JasonOsborn <47375292+JasonOsborn@users.noreply.github.com>
2019-10-02 13:06:06 -0500

    Add files via upload

--- rpi_thermal_cam.py
+++ rpi_thermal_cam.py
@@ -1,20 +1,28 @@
 """This example is for Raspberry Pi (Linux) only!
    It will not work on microcontrollers running CircuitPython!"""
+#!/usr/bin/env python3
 
 import os
 import math
 import time
-
+import datetime
 import busio
 import board
+import sys
+
+#from signal import pause
 
 import numpy as np
 import pygame
 from scipy.interpolate import griddata
 
 from colour import Color
-
+import RPi.GPIO as GPIO
 import adafruit_amg88xx
+
+#GPIO.setmode(GPIO.BOARD)
+GPIO.setup(17, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(23, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 i2c_bus = busio.I2C(board.SCL, board.SDA)
 
@@ -22,7 +30,7 @@ i2c_bus = busio.I2C(board.SCL, board.SDA)
 MINTEMP = 26.
 
 #high range of the sensor (this will be red on the screen)
-MAXTEMP = 32.
+MAXTEMP = 28.
 
 #how many color values we can have
 COLORDEPTH = 1024
@@ -35,12 +43,12 @@ sensor = adafruit_amg88xx.AMG88XX(i2c_bus)
 
 # pylint: disable=invalid-slice-index
 points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
-grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j] #0:7:32 original
 # pylint: enable=invalid-slice-index
 
 #sensor is an 8x8 grid so lets do a square
-height = 240
-width = 240
+height = 420#240x240 original
+width = 640 #pygame window size
 
 #the list of colors we can choose from
 blue = Color("indigo")
@@ -49,12 +57,12 @@ colors = list(blue.range_to(Color("red"), COLORDEPTH))
 #create the array of colors
 colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
 
-displayPixelWidth = width / 30
-displayPixelHeight = height / 30
+displayPixelWidth = width / 20 #30 original
+displayPixelHeight = height / 20 #30 original
 
 lcd = pygame.display.set_mode((width, height))
 
-lcd.fill((255, 0, 0))
+lcd.fill((255, 0, 0)) #255 orginal
 
 pygame.display.update()
 pygame.mouse.set_visible(False)
@@ -71,7 +79,7 @@ def map_value(x, in_min, in_max, out_min, out_max):
 
 #let the sensor initialize
 time.sleep(.1)
-
+ 
 while True:
 
     #read the pixels
@@ -89,5 +97,22 @@ while True:
             pygame.draw.rect(lcd, colors[constrain(int(pixel), 0, COLORDEPTH- 1)],
                              (displayPixelHeight * ix, displayPixelWidth * jx,
                               displayPixelHeight, displayPixelWidth))
-
+ 
     pygame.display.update()
+    
+    if(GPIO.input(17) == 0):
+        now = datetime.datetime.now()#displays cur time
+        str_now = str(now)
+        d_str = str_now[5:10]
+        d_str1 = str_now[11:19]#f_date displays m-d_h-m-s time
+        f_date = d_str+"_"+d_str1[0:2]+"-"+d_str1[3:5]+\
+                 "-"+d_str1[6:8]
+        image_file = "scrot /media/pi/5BD7-E1141/sample"\
+                     +f_date+".png"    
+        os.system(image_file)
+        
+        print("f_date = ")
+        print(f_date)
+        print(str_now)
+    if(GPIO.input(23) == 0):
+        sys.exit()
